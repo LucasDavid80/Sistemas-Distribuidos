f3fd6f7421700323cf791186213805ff6780864f
Lucas David <lucasdavidoliveira80@gmail.com>
2026-08-28 16:04:56 -0300

    test(backend): adiciona testes unitarios com pytest e rota personalizada

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
     return {"message": "Olá, Sistemas Distribuídos!"}
+
+
+@app.get("/lucas")
+def hello_lucas():
+    return {"message": "Olá, Lucas!"}
